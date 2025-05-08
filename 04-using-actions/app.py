69c716c039f423d3bc8609f0d8c6cd565bfa08f7
Jakub Nowak <79985746+jakubno3ak@users.noreply.github.com>
2025-05-08 16:52:22 +0000

    Fi ruff formating in app

--- 04-using-actions/app.py
+++ 04-using-actions/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from src.inference import Inference
 from settings import settings
 from src.app.models import PredictSentimentResponse, PredictSentimentRequest
@@ -11,9 +11,11 @@ inference = Inference(settings=settings)
 async def health():
     return 200
 
+
 @app.get("/")
 async def home():
     return {"INFO": "Go to '/docs' endpoint"}
+
 
 @app.post("/predict")
 async def predict(request: PredictSentimentRequest):
